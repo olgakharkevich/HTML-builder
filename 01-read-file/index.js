041ed7a4ee35982167d739d243674851d7d635ea
Olga Kharkevich <olga.kharkevich@gmail.com>
2022-11-02 04:32:04 -0400

    feat: add solution for 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const stream =  fs.createReadStream(path.join(__dirname, '/text.txt'), 'utf8');
+
+stream.on('data', (data) => {
+    console.log(data);
+});
+
+stream.on('error', (error) => {
+    console.log(error);
+});
